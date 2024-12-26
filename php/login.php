88e4c1d103a1da3305878be221e6958b2f1fa98b
MathiMV <139645735+MathiMV@users.noreply.github.com>
2024-12-26 16:50:53 +0000

    create login php

--- php/login.php
+++ php/login.php
@@ -1,28 +1,36 @@
 <?php
-    session_start();
-    require_once 'dbconf.php';
+require_once 'dbconf.php';
 
-        if (isset($_POST['login'])) {
+if (isset($_POST['login'])) {
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+    
+    $sql = "SELECT username, password FROM user WHERE username = '$username'";
+    $result = $connect->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"]; 
+
+      
+        if (password_verify($password, $hashedPassword)) {
+            session_start(); 
+        
+           // $_SESSION["user_name"] = $row["username"];
+           // $_SESSION['role'] = $row['role'];
             
-            $name = $_POST["name"];
-            $upassword = $_POST["password"];
-            $hashed_password = password_hash($upassword,PASSWORD_DEFAULT);
-
-            $sql = "SELECT username FROM user WHERE username = '$name' AND password = '$hashed_password'";
-            
-            $result = $connect->query($sql);
-
-            if ($result ->num_rows > 0) {
-               // session_start(); // you can add this at the start line of the document
-                
-                $row = $result -> fetch_assoc();
-                $_SESSION["user_name"] = $row["username"];
-                header('Location: ../index.html');
-            } 
-            else {
-                echo '<p class="error-message">Invalid username or password.</p>';
-            }
-            $connect->close();
+    
+                header("Location: ../index.html");
+                exit; 
+        } else {
+            $error = "Invalid username or password.";
         }
-    ?>
+    } else {
+        $error = "Invalid username or password.";
+    }
 
+    $connect->close(); // Close the database connection
+    header("Location: ../login.html?error=" . urlencode($error));
+    exit;
+}
+?>
